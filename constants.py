8b91f14ca89b1abced42e1648d76a6bffca3d09f
KyDang352541 <vinhkyag@gmail.com>
2025-07-31 07:18:41 +0700

    Update constants.py

--- constants.py
+++ constants.py
@@ -2,9 +2,9 @@
 
 # Fixed hourly rates (USD)
 HOURLY_RATES = {
-    "Worker": 5.0,
-    "Office": 10.0,
-    "CNC": 20.0,
-    "Robot": 25.0,
-    "Autoclave": 30.0,
+    "Worker": 13.41,
+    "Office": 31.25,
+    "CNC": 18.33,
+    "Robot": 49.98,
+    "Autoclave": 19.79,
 }
